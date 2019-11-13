0c0cfa6bc09976b1ca1b4f5a3db1bab0f07149ae
Bruno Maciel Duarte <bruno.duarte@unyleya.com.br>
2019-11-13 18:24:08 -0300

    Mundando aquivo env para config

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    googleAPi: {
+
+        mapsDistanceMatrix: {
+            url: "https://maps.googleapis.com/maps/api/distancematrix/json",
+            units: "imperial",
+            language: "pt-BR",
+            key: ""
+        }
+
+    }
+
+};
